3ac60ec25b61f8eab2c8fec5384b4406a1c1a298
edmilsonslima <412368edmilson_santos_lima@hotmail.com>
2015-11-11 21:08:12 -0300

    Seed de departamentos melhorada.

--- database/seeds/DepartamentosTableSeeder.php
+++ database/seeds/DepartamentosTableSeeder.php
@@ -12,16 +12,16 @@ class DepartamentosTableSeeder extends Seeder
     public function run()
     {
         DB::table('departamentos')->insert(['titulo' => 'DCC - Departamento de Ciência da Computação',]);
-        DB::table('departamentos')->insert(['titulo' => 'Departamento de Ciência e Tecnologia dos Materiais',]);
-        DB::table('departamentos')->insert(['titulo' => 'Departamento de Engenharia Elétrica',]);
+        DB::table('departamentos')->insert(['titulo' => 'DCTM - Departamento de Ciência e Tecnologia dos Materiais',]);
+        DB::table('departamentos')->insert(['titulo' => 'DEE - Departamento de Engenharia Elétrica',]);
         DB::table('departamentos')->insert(['titulo' => 'DEM - Departamento de Engenharia Mecânica',]);
-        DB::table('departamentos')->insert(['titulo' => 'Departamento de Engenharia Química',]);
-        DB::table('departamentos')->insert(['titulo' => 'Departamento de Matemática',]);
+        DB::table('departamentos')->insert(['titulo' => 'DEQ - Departamento de Engenharia Química',]);
+        DB::table('departamentos')->insert(['titulo' => 'DMAT - Departamento de Matemática',]);
         DB::table('departamentos')->insert(['titulo' => 'DPFILO - Departamento de Filosofia',]);
-        DB::table('departamentos')->insert(['titulo' => 'Departamento de Letras Vernáculas',]);
-        DB::table('departamentos')->insert(['titulo' => 'Departamento de Produção Animal',]);
-        DB::table('departamentos')->insert(['titulo' => 'Departamento de Patologia e Clínicas',]);
-        DB::table('departamentos')->insert(['titulo' => 'Departamento de Anestesiologia e Cirurgia',]);
+        DB::table('departamentos')->insert(['titulo' => 'DLV - Departamento de Letras Vernáculas',]);
+        DB::table('departamentos')->insert(['titulo' => 'DPA - Departamento de Produção Animal',]);
+        DB::table('departamentos')->insert(['titulo' => 'DPC - Departamento de Patologia e Clínicas',]);
+        DB::table('departamentos')->insert(['titulo' => 'DAC - Departamento de Anestesiologia e Cirurgia',]);
 
     }
 }
